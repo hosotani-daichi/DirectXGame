b405f0d2ae5246fc4ce948a39caf3c406799497e
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2024-04-26 11:16:17 +0900

    02_01

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,66 +1,19 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-#include "ImGuiManager.h"
-
 
 GameScene::GameScene() {}
 
-//デストラクタ
-GameScene::~GameScene() { 
-	delete sprite_; 
-	delete model_;
-	delete debugCamera_;
-
-}
+GameScene::~GameScene() {}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
-
-	// ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("soup_wakame.png");
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-
-	//3Dモデルの生成
-	model_ = Model::Create();
-
-	//ワールドトランスフォームの初期化
-	worldTransform_.Initialize();
-	//ビュープロジェクションの初期化
-	viewProjection_.Initialize();
-
-	//デバッグカメラの生成
-	debugCamera_ = new DebugCamera(1280, 720);
-
 }
 
-//更新
-void GameScene::Update() {
-	//スプライトの今の座標を取得
-	Vector2 position = sprite_->GetPosition();
-	//座標を{2,1}移動
-	position.x += 2.0f;
-	position.y += 1.0f;
-	//移動した座標をスプライトに反映
-	sprite_->SetPosition(position);
-
-	//デバックテキストの表示	
-	ImGui::Begin("Debug1");
-	//float3入力ボックス
-	ImGui::InputFloat3("InputFloat3", inputFloat3);
-	//float3スライダー
-	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
-	ImGui::Text("kamata tarou %d.%d.%d", 2050, 12, 31);
-	ImGui::End();
-	//デモウィンドウの表示を有効化
-	ImGui::ShowDemoWindow();
-	//デバッグカメラの更新
-	debugCamera_->Update();
-
-}
+void GameScene::Update() {}
 
 void GameScene::Draw() {
 
@@ -89,11 +42,6 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
-	//3Dモデル描画
-	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
-
-	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
-
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -105,7 +53,6 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	sprite_->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
